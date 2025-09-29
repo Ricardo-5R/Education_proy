95fdc74b8f58750684fde48868578b149f9f8397
Ricardo-5R <richard-08512@outlook.com>
2025-09-29 16:50:17 -0500

    Update sidebar.jsx
    simulado

--- frontend/education/src/components/sidebar.jsx
+++ frontend/education/src/components/sidebar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import {
   FaClipboardList,
   FaGraduationCap,
@@ -8,10 +8,15 @@ import {
   FaChartBar,
   FaCog,
   FaComments,
-} from "react-icons/fa"
-import "../stylescomponents/sidebar.css"
+} from "react-icons/fa";
+import "../stylescomponents/sidebar.css";
 
 const Sidebar = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  // Validar si hay sesión activa y si el usuario es docente
+  if (!user || !user.docente) return null;
+
   return (
     <div className="sidebar-container">
       <div className="sidebar-header">
@@ -40,7 +45,7 @@ const Sidebar = () => {
             </Link>
           </li>
 
-          {/* Chats  */}
+          {/* Chats */}
           <li className="sidebar-item">
             <Link to="/chats" className="sidebar-link">
               <div className="sidebar-icon">
@@ -59,13 +64,40 @@ const Sidebar = () => {
               <div className="sidebar-text">Estudiantes</div>
             </Link>
           </li>
-        </ul>
 
-        
+          {/* Grupos */}
+          <li className="sidebar-item">
+            <Link to="/grupos" className="sidebar-link">
+              <div className="sidebar-icon">
+                <FaBook />
+              </div>
+              <div className="sidebar-text">Grupos</div>
+            </Link>
+          </li>
+
+          {/* Publicaciones */}
+          <li className="sidebar-item">
+            <Link to="/publicaciones" className="sidebar-link">
+              <div className="sidebar-icon">
+                <FaChartBar />
+              </div>
+              <div className="sidebar-text">Publicaciones</div>
+            </Link>
+          </li>
+
+          {/* Configuración */}
+          <li className="sidebar-item">
+            <Link to="/configuracion" className="sidebar-link">
+              <div className="sidebar-icon">
+                <FaCog />
+              </div>
+              <div className="sidebar-text">Configuración</div>
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
-
+export default Sidebar;
